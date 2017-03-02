b1adc3435decc189f40e5368747ae1fca7bf356e
Dominique Roduit <dominique.roduit@epfl.ch>
2017-03-02 23:09:21 +0100

    part.2 - Checkeboard

--- hw2p2_checkerboard/quad/quad_fshader.glsl
+++ hw2p2_checkerboard/quad/quad_fshader.glsl
@@ -1,4 +1,5 @@
 #version 330
+#define M_PI 3.1415926535897932384626433832795
 
 in vec2 uv;
 
@@ -6,8 +7,21 @@ out vec3 color;
 
 uniform sampler1D colormap;
 
+
 void main() {
-    color = vec3(1.0, 0.0, 0.0);
+
+    float freq = 5 * (2*M_PI);
+    float value =  (sin(uv.x * freq) * sin(uv.y * freq) + 1) / 2;
+    color = texture(colormap, value).rgb;
+
+    /*
+    an (experimental) alternative (approximately)
+    color = vec3(
+          1 - sin(10.0 * M_PI * uv.y) * sin(10.0 * M_PI * uv.x),
+          1 - sin(10.0 * M_PI * uv.y + M_PI) * sin(10.0 * M_PI * uv.x),
+          0.0
+      );
+    */
 }
 
 
