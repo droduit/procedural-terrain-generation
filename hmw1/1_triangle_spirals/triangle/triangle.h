787aed5bee3ad654c2f4dc49dd3247b7ec694e65
Dominique Roduit <dominique.roduit@epfl.ch>
2017-03-02 17:16:26 +0100

    suppression du dossier nul de depart

--- hmw1/1_triangle_spirals/triangle/triangle.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class Triangle {
-private:
-
-public:
-    Triangle();
-    virtual ~Triangle();
-};
--- hmw1/1_triangle_spirals/triangle/triangle_vshader.glsl
+++
@@ -1,7 +0,0 @@
-#version 330
-
-in vec3 vpoint;
-
-void main() {
-    gl_Position = vec4(vpoint, 1.0);
-}
