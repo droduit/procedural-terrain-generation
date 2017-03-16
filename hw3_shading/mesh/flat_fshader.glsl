fcb6b7b3ca3344b00f245f86201b111e3e41c5fe
Romain Gehrig <romain.gehrig@gmail.com>
2017-03-16 20:21:27 +0100

    Add flat shading

--- hw3_shading/mesh/flat_fshader.glsl
+++ hw3_shading/mesh/flat_fshader.glsl
@@ -1,26 +1,30 @@
 #version 330
 
 
+in vec4 vpoint_mv;
+in vec3 light_dir, view_dir;
+
+uniform vec3 La, Ld, Ls;
+uniform vec3 ka, kd, ks;
+uniform float alpha;
+
 out vec3 color;
 
-
 void main() {
-    color = vec3(0.0,0.0,0.0);
-    const vec3 COLORS[6] = vec3[](
-        vec3(1.0,0.0,0.0),
-        vec3(0.0,1.0,0.0),
-        vec3(0.0,0.0,1.0),
-        vec3(1.0,1.0,0.0),
-        vec3(0.0,1.0,1.0),
-        vec3(1.0,0.0,1.0));
-    int index = int( mod(gl_PrimitiveID,6) );
-    color = COLORS[index];
+    // The only difference from the Phong shading is that we
+    // compute the normal vector differently
+    vec3 dx = dFdx(vpoint_mv.xyz);
+    vec3 dy = dFdy(vpoint_mv.xyz);
+    vec3 normal_mv = normalize(cross(dx,dy));
 
-    ///>>>>>>>>>> TODO >>>>>>>>>>>
-    /// TODO 4.2: Flat shading.
-    /// 1) compute triangle normal using dFdx and dFdy
-    /// 1) compute ambient term.
-    /// 2) compute diffuse term.
-    /// 3) compute specular term.
-    ///<<<<<<<<<< TODO <<<<<<<<<<<
+    color += La*ka;
+    vec3 n = normalize(normal_mv);
+    vec3 l = normalize(light_dir);
+    float lambert = dot(n,l);
+    if(lambert > 0.0) {
+        color += Ld*kd*lambert;
+        vec3 v = normalize(view_dir);
+        vec3 r = reflect(-l,n);
+        color += Ls*ks*pow(max(dot(r,v), 0.0), alpha);
+    }
 }
--- hw3_shading/mesh/flat_vshader.glsl
+++ hw3_shading/mesh/flat_vshader.glsl
@@ -14,9 +14,7 @@ void main() {
     mat4 MV = view * model;
     vpoint_mv = MV * vec4(vpoint, 1.0);
     gl_Position = projection * vpoint_mv;
-    //>>>>>>>>>> TODO >>>>>>>>>>>
-    // TODO 4.1: Flat shading.
-    // 1) compute the light direction light_dir.
-    // 2) compute the view direction view_dir.
-    //<<<<<<<<<< TODO <<<<<<<<<<<
+
+    light_dir = light_pos - vpoint_mv.xyz;
+    view_dir = -vpoint_mv.xyz;
 }
