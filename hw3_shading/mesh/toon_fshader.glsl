510fadad8faccc49f8884b1971309101f7d0a185
Thierry Treyer <zehkae@gmail.com>
2017-03-16 19:56:44 +0100

    Clamp texcoord for toon shader

--- hw3_shading/mesh/toon_fshader.glsl
+++ hw3_shading/mesh/toon_fshader.glsl
@@ -26,13 +26,13 @@ void main() {
     vec3 light_dir = normalize(light_dir);
     vec3 view_dir = normalize(view_dir);
 
-    float lambert =  dot(normal_mv, light_dir);
-    float rv = dot(reflect(-light_dir, normal_mv), view_dir);
+    float lambert = clamp(dot(normal_mv, light_dir), 0.0, 1.0);
+    float rv = clamp(dot(reflect(-light_dir, normal_mv), view_dir), 0.0, 1.0);
 
     vec3 ambient = La * ka;
 
     vec3 diffuse = vec3(0.0), specular = vec3(0.0);
-    if(lambert > 0.0) {
+    if (lambert > 0.0) {
         diffuse = Ld * kd * texture(tex1D, lambert).r;
         specular = Ls * ks * texture(tex1D, pow(clamp(rv, 0.0, 1.0), alpha)).r;
     }
