9e4dc598f3be2b75282d9ef612dc51ac669b6214
Thierry Treyer <zehkae@gmail.com>
2017-03-16 17:28:11 +0100

    Implement spotlight

--- hw3_shading/mesh/spot_vshader.glsl
+++ hw3_shading/mesh/spot_vshader.glsl
@@ -1,17 +1,23 @@
 #version 330
 
-in vec3 vpoint;
-in vec3 vnormal;
-
-
 uniform mat4 projection;
 uniform mat4 model;
 uniform mat4 view;
 uniform vec3 light_pos;
-uniform vec3 spot_dir;
+
+in vec3 vpoint;
+in vec3 vnormal;
+
+out vec3 normal_mv;
+out vec3 light_dir;
+out vec3 view_dir;
 
 void main() {
     mat4 MV = view * model;
     vec4 vpoint_mv = MV * vec4(vpoint, 1.0);
     gl_Position = projection * vpoint_mv;
+
+    normal_mv = mat3(inverse(transpose(MV))) * vnormal;
+    light_dir = light_pos - vpoint_mv.xyz;
+    view_dir = -vpoint_mv.xyz;
 }
