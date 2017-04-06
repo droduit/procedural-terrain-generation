a73911bf1ad6df66c296c767e12a19df9b9d1c12
Dominique Roduit <dominique.roduit@epfl.ch>
2017-04-06 16:53:32 +0200

    Modif des parametres de la texture

--- project/terrain/terrain.h
+++ project/terrain/terrain.h
@@ -126,6 +126,8 @@ class Terrain {
                 glBindTexture(GL_TEXTURE_2D, texture_color_id_);
                 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
                 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
                 if(nb_component == 3) {
                     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0,
